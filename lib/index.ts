24eb84e813431abe3509fe1b6ff0d3ccd52af5aa
Alex Couturon <infocontact.alex@gmail.com>
2020-02-13 17:03:02 +0100

    Expose types

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,13 @@
 import ApiStorePlugin from './ApiStorePlugin';
 import ApiState from './ApiState';
 import DateTimeState from './DateTimeState';
+import { ModelTypeTree, Payload, QueuePayload } from './types';
 
-export { ApiStorePlugin, ApiState, DateTimeState };
+export {
+  ApiStorePlugin,
+  ApiState,
+  DateTimeState,
+  ModelTypeTree,
+  Payload,
+  QueuePayload
+};
